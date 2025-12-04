154c8c25ee2a0229818f1b9e92d7997672626c47
Bayu Ramadeza <bayu.ramadeza@gramedia.id>
2025-12-04 14:17:05 +0700

    feat: add panel_animation_navigation

---
+++ lib/v2/components/navigations/panel_animation_navigation.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class PanelNavigationAnimation<T> extends StatefulWidget {
+  const PanelNavigationAnimation({
+    super.key,
+    required this.offset,
+    required this.width,
+    required this.height,
+    required this.hasTouch,
+    required this.duration,
+    required this.curve,
+    this.decoration,
+  });
+
+  final double offset;
+  final double? width;
+  final double? height;
+  final Duration duration;
+  final Curve curve;
+  final bool hasTouch;
+  final BoxDecoration? decoration;
+
+  @override
+  State<PanelNavigationAnimation<T>> createState() =>
+      _PanelNavigationAnimationState<T>();
+}
+
+class _PanelNavigationAnimationState<T>
+    extends State<PanelNavigationAnimation<T>> {
+  @override
+  @override
+  Widget build(BuildContext context) {
+    final isRtl = Directionality.of(context) == TextDirection.rtl;
+    final offset = isRtl ? widget.offset * -1 : widget.offset;
+    return AnimatedContainer(
+      transform: Matrix4.translationValues(offset, 0, 0),
+      duration: widget.duration,
+      curve: widget.curve,
+      width: widget.width,
+      height: widget.height,
+      decoration: widget.decoration,
+    );
+  }
+}
